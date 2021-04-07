826b40bcbacc2114c981d08555bda66caef059bc
peixinguo <peixin.guo24@gmail.com>
2021-04-07 23:08:50 +0800

    observer pattern

---
+++ src/patterns/observer/Observer.java
@@ -0,0 +1,5 @@
+package patterns.observer;
+//观察者接口
+public interface Observer {
+    public void update(float temp, float humidity, float pressure);
+}
---
+++ src/patterns/observer/Subject.java
@@ -0,0 +1,8 @@
+package patterns.observer;
+//主题接口
+public interface Subject {
+
+    public void registerObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObservers();
+}
